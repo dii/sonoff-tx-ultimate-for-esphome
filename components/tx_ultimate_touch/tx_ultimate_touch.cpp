daa3d01d22d28f28825cdb9cbc789c4f881a5d13
Dawid Oosthuizen <dawid.oosthuizen@gmail.com>
2024-04-29 15:27:50 +1000

    left to 1 and right to 2

--- components/tx_ultimate_touch/tx_ultimate_touch.cpp
+++ components/tx_ultimate_touch/tx_ultimate_touch.cpp
@@ -87,14 +87,14 @@ namespace esphome
                 this->touch_trigger_.trigger(tp);
                 break;
 
-            case TOUCH_STATE_SWIPE_LEFT:
+            case TOUCH_STATE_SWIPE_1:
                 ESP_LOGD(TAG, "Swipe Left (x=%d)", tp.x);
-                this->swipe_trigger_left_.trigger(tp);
+                this->swipe_trigger_1_.trigger(tp);
                 break;
 
-            case TOUCH_STATE_SWIPE_RIGHT:
+            case TOUCH_STATE_SWIPE_2:
                 ESP_LOGD(TAG, "Swipe Right (x=%d)", tp.x);
-                this->swipe_trigger_right_.trigger(tp);
+                this->swipe_trigger_2_.trigger(tp);
                 break;
 
             case TOUCH_STATE_ALL_FIELDS:
@@ -119,8 +119,8 @@ namespace esphome
             int state = get_touch_state(bytes);
             if (state != TOUCH_STATE_PRESS &&
                 state != TOUCH_STATE_RELEASE &&
-                state != TOUCH_STATE_SWIPE_LEFT &&
-                state != TOUCH_STATE_SWIPE_RIGHT &&
+                state != TOUCH_STATE_SWIPE_1 &&
+                state != TOUCH_STATE_SWIPE_2 &&
                 state != TOUCH_STATE_ALL_FIELDS)
             {
                 return false;
@@ -147,11 +147,11 @@ namespace esphome
                 return bytes[5];
                 break;
 
-            case TOUCH_STATE_SWIPE_LEFT:
+            case TOUCH_STATE_SWIPE_1:
                 return bytes[5];
                 break;
 
-            case TOUCH_STATE_SWIPE_RIGHT:
+            case TOUCH_STATE_SWIPE_2:
                 return bytes[5];
                 break;
 
@@ -177,13 +177,13 @@ namespace esphome
 
             if (state == TOUCH_STATE_SWIPE)
             {
-                if (bytes[5] == TOUCH_STATE_SWIPE_RIGHT)
+                if (bytes[5] == TOUCH_STATE_SWIPE_2)
                 {
-                    state = TOUCH_STATE_SWIPE_RIGHT;
+                    state = TOUCH_STATE_SWIPE_2;
                 }
-                else if (bytes[5] == TOUCH_STATE_SWIPE_LEFT)
+                else if (bytes[5] == TOUCH_STATE_SWIPE_1)
                 {
-                    state = TOUCH_STATE_SWIPE_LEFT;
+                    state = TOUCH_STATE_SWIPE_1;
                 }
             }
 
